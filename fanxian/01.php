3afd48c80dfd88427501f6f8ffbfb34b6e1ddb02
guoruitao <guoruitao@tvmining.com>
2016-08-09 09:29:01 +0800

    返现

---
+++ fanxian/01.php
@@ -0,0 +1,16 @@
+<?php
+
+echo '[{"headImg":"./images/tx.jpg","name":"黄晓明","place":"商城购物","money":"返现180元","time":"刚刚"}
+      ,{"headImg":"./images/tx.jpg","name":"黄晓明","place":"商城购物","money":"返现180元","time":"刚刚"}
+      ,{"headImg":"./images/tx.jpg","name":"黄晓明","place":"商城购物","money":"返现180元","time":"刚刚"}
+      ,{"headImg":"./images/tx.jpg","name":"黄晓明","place":"商城购物","money":"返现180元","time":"刚刚"}
+      ,{"headImg":"./images/tx.jpg","name":"黄晓明","place":"商城购物","money":"返现180元","time":"刚刚"}
+      ,{"headImg":"./images/tx.jpg","name":"黄晓明","place":"商城购物","money":"返现180元","time":"刚刚"}
+      ,{"headImg":"./images/tx.jpg","name":"黄晓明","place":"商城购物","money":"返现180元","time":"刚刚"}
+      ,{"headImg":"./images/tx.jpg","name":"黄晓明","place":"商城购物","money":"返现180元","time":"刚刚"}
+      ,{"headImg":"./images/tx.jpg","name":"黄晓明","place":"商城购物","money":"返现180元","time":"刚刚"}
+      ,{"headImg":"./images/tx.jpg","name":"黄晓明","place":"商城购物","money":"返现180元","time":"刚刚"}
+        ]';
+sleep(1);
+
+?>
---
+++ fanxian/main.js
@@ -0,0 +1,3 @@
+/**
+ * Created by TVM on 2016/8/8.
+ */
